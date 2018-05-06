f32c1d945b29f68d6255a6319dab8951d5f3c784
DESKTOP-EEIMSU0\K <kentyn1998@gmail.com>
2018-05-06 11:48:21 -0500

    First Commit
    First commit with 2018.1 Unity

---
+++ Siege/Assets/00 - Scripts/Roofblock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roofblock : MonoBehaviour {
+
+    public GameObject rooftop;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            Destroy(rooftop);
+        }
+        
+    }
+}
